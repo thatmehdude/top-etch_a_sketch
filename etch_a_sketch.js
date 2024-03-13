0f16d6a84a65805adcf63cc5455ef7b7a56c48aa
Chisom <anaelechisom16@gmail.com>
2024-03-13 14:04:56 +0100

    display random color on hover

--- etch_a_sketch.js
+++ etch_a_sketch.js
@@ -6,6 +6,13 @@ const sketchArea = document.querySelector("#sketchArea");
 sketchArea.style.width = `${GRIDSIZE}px`;
 sketchArea.style.height = `${GRIDSIZE}px`;
 
+function setBackgroundColor(){
+    let randomRed = Math.floor(Math.random() * 256);
+    let randomGreen = Math.floor(Math.random() * 256);
+    let randomBlue = Math.floor(Math.random() * 256);
+    let randomColour = "rgb(" + randomRed + "," + randomGreen + "," + randomBlue + ")";
+    this.style.backgroundColor = randomColour;
+}
 function createGridCells(){
     for (let i = 0; i < (rows * columns); i++){
         const gridCell = document.createElement("div");
@@ -15,6 +22,7 @@ function createGridCells(){
         gridCell.classList.add("cell");
 
         sketchArea.appendChild(gridCell);
+        gridCell.addEventListener('mouseover', setBackgroundColor);
     }
 }
 
